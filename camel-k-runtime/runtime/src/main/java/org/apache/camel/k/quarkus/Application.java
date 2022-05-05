92d37f32d28e0b4a9338b616a6f7bd5da59e366b
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-05-05 12:36:45 +0200

    Updated NoRoutesCollector to match the updated interface from Camel

--- camel-k-runtime/runtime/src/main/java/org/apache/camel/k/quarkus/Application.java
+++ camel-k-runtime/runtime/src/main/java/org/apache/camel/k/quarkus/Application.java
@@ -33,6 +33,7 @@ import org.apache.camel.RoutesBuilder;
 import org.apache.camel.main.BaseMainSupport;
 import org.apache.camel.main.MainListener;
 import org.apache.camel.main.RoutesCollector;
+import org.apache.camel.spi.Resource;
 
 public final class Application {
     private Application() {
@@ -148,6 +149,11 @@ public final class Application {
 
         @Override
         public List<RoutesBuilder> collectRoutesFromRegistry(CamelContext camelContext, String excludePattern, String includePattern) {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public Collection<Resource> findRouteResourcesFromDirectory(CamelContext camelContext, String excludePattern, String includePattern) {
             return Collections.emptyList();
         }
     }
